9c91ee1f7636b5006d99edb26511c51aad615b65
Daniel <daniel.won.choi@gmail.com>
2019-04-25 22:17:32 -0500

    gg done except feature

--- data/projects/semesters/sp19/gg.js
+++ data/projects/semesters/sp19/gg.js
@@ -25,98 +25,82 @@ const clientFeatures = [
     "Currently, GlobalGiving connects with organizations based in the US along with some nonprofits in other countries. However, the process of finding and applying to GlobalGiving remains significantly easier within the United States. In certain countries, factors including lack of internet connectivity and lack of access to documents required by GlobalGiving has led to slower onboarding and discovery of the organization.";
   const problemImagePath = "/static/images/projects/gg/gg-cover.jpg";
 const solution =
-  "We want to educate students about what’s around them. If we give them the information they need about what’s around campus, they will be better prepared to make decisions on how they should get home. From our research, we discovered that many students value knowing about where emergency phones, open businesses, lighting, past crimes, and other people are. For the app, we focused on giving students information about open businesses, emergency phones, police stations, street lights, bus stops, and past crimes are. ";
-const solutionImagePath = "/static/images/projects/question.jpg";
-const features = [
-  {
-    title: "Location and Map Services",
-    detail:
-      "Users can browse the map, know their current location, and zoom back to their current location using their phone’s native map application.",
-    imgPath: "/static/images/projects/c2tc-2/c2tc-2-map.png"
-  },
-  {
-    title: "Filter Data",
-    detail:
-      "The C2TC app has location information on U of I’s bus stops, crimes in the past month, open businesses, emergency phones, police stations, street lights and keeps these updated constantly. ",
-    imgPath: "/static/images/projects/c2tc-2/c2tc-2-filters.png"
-  },
-  {
-    title: "Applying Filters",
-    detail:
-      "Users can then apply filters to see how dangerous the surrounding area and what the best path to take home at night.",
-    imgPath: "/static/images/projects/c2tc-2/c2tc-2-apply-filters.png"
-  },
-  {
-    title: "Safe Rides and Safe Walks Integration",
-    detail:
-      "Users can quickly call safe rides and safe walks within the app if they are not comfortable walking home alone.",
-    imgPath: "/static/images/projects/c2tc-2/c2tc-2-safe-rides.png"
-  }
-];
-
-const featureImgSize = "4";
-const techStack = ["reactnative", "flask", "mongodb"];
-const quote =
-  "We talked to a couple of people about developing about Cut to the Case, but a lot of them didn’t have the same passion that Hack4Impact had for the project. Hack4Impact had enthusiasm for what we were doing.";
-const quoteSource = "Premika Pandian";
-const quoteSourceTitle = "Cut to the Case Co-Founder and CTO";
-const githubLink = "https://github.com/hack4impact-uiuc/c2tc-fall-2018";
-const team = {
-  type: "pics",
-  detail: [
+  "We are working to uncover ways of characterizing the work that nonprofit organizations do all over the world. We've built tools which analyze the websites and missions statements of nonprofits, group them together according to what they do, and give suggestions as to what each organization’s work may be. Ultimately, we hopes to give GlobalGiving as much knowledge and foresight as possible when they reach out to new nonprofits to  join their global network.";
+  const solutionImagePath = "/static/images/projects/gg/gg-crawl.png";
+  const features = [
     {
-      name: "Shreyas Mohan",
-      role: "Product Manager"
+      title: "Data collection through scraping",
+      detail:
+        "Choose ten websites with directories of nonprofits and scrape each individual one and add them to the database. Data includes the organization’s name, country of registration, registration numbers, website url, and email.",
+      imgPath: "/static/images/projects/gg/gg-scraper.png"
     },
     {
-      name: "Megha Mallya",
-      role: "Tech Lead"
+      title: "Command Line Tool",
+      detail:
+        "Allows user to see all the available scripts, run them, and submit the entries to the global giving database using the organization parser",
+      imgPath: "/static/images/projects/gg/gg-run.png"
     },
     {
-      name: "Josh Burke",
-      role: "Software Developer"
-    },
-    {
-      name: "Neeraj Aggarwal",
-      role: "Software Developer"
-    },
-    {
-      name: "Daniel Choi",
-      role: "Software Developer"
-    },
-    {
-      name: "Anooj Lal",
-      role: "Software Developer"
-    },
-    {
-      name: "Phillip Kuo",
-      role: "UI/UX Designer"
-    },
-    {
-      name: "Annie Wu",
-      role: ""
+      title: "Web Crawler",
+      detail:
+        "Crawls the web and google searches and ranks potential NGO directories",
+      imgPath: "/static/images/projects/gg/gg-crawl.png"
     }
-  ]
-};
-
-export default {
-  name,
-  id,
-  detail,
-  coverImagePath,
-  clientDetail,
-  clientLink,
-  clientFeatures,
-  problem,
-  problemImagePath,
-  solution,
-  solutionImagePath,
-  features,
-  techStack,
-  quote,
-  quoteSource,
-  quoteSourceTitle,
-  githubLink,
-  team,
-  featureImgSize
-};
+  ];
+  const featureImgSize = "7";
+  const techStack = ["sklearn", "gensim", "nltk", "beautifulsoup", "dynamodb", "aws", "spacy"];
+  const quote = null;
+  const quoteSource = null;
+  const quoteSourceTitle = null;
+  const githubLink = "https://github.com/hack4impact-uiuc/globalgiving-depth";
+  const team = {
+    type: "pic",
+    detail: [
+      {
+        name: "Josh Burke",
+        role: "Product Manager"
+      },
+      {
+        name: "Aryn Harmon",
+        role: "Tech Lead"
+      },
+      {
+        name: "Prashant Pokhriyal",
+        role: "Software Developer"
+      },
+      {
+        name: "Lam Tran",
+        role: "Software Developer"
+      },
+      {
+        name: "Eugenia Chen",
+        role: "Software Developer"
+      },
+      {
+        name: "Jacqueline Osborn",
+        role: "Software Developer"
+      }
+    ]
+  };
+  export default {
+    name,
+    id,
+    detail,
+    coverImagePath,
+    clientDetail,
+    clientLink,
+    clientFeatures,
+    problem,
+    problemImagePath,
+    solution,
+    solutionImagePath,
+    features,
+    techStack,
+    quote,
+    quoteSource,
+    quoteSourceTitle,
+    githubLink,
+    team,
+    featureImgSize
+  };
+  
